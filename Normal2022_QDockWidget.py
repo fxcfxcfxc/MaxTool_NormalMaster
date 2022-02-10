b39935b31a4a0c282bbce80290ed5166c09ae803
fxcfxcfxc <49788446+fxcfxcfxc@users.noreply.github.com>
2022-02-10 15:18:29 +0800

    add

--- Normal2022_QDockWidget.py
+++ Normal2022_QDockWidget.py
@@ -1,38 +1,36 @@
 import os
 from PySide2.QtWidgets import QVBoxLayout
 from PySide2.QtWidgets import QWidget
-from PySide2.QtWidgets import QDialog
+from PySide2.QtWidgets import QDockWidget
 from PySide2.QtWidgets import QPushButton,QRadioButton,QSlider
 from PySide2.QtCore import QFile
+from PySide2 import QtCore
 from PySide2.QtUiTools import QUiLoader
+import qtmax
 from pymxs import runtime as rt
-
 '''
 3dmax2022
-继承QDialog 版本，窗口属于3dmax同级面板
+继承QDockWidget 版本，窗口属于3dmax工具面板
 
 
 '''
 
 #继承Qdialog类，代表对话框类
-class TestDialog(QDialog):
+class TestDialog(QDockWidget):
     #TestDialog类的构造函数，传入参数，参数为类类型
-    def __init__(self, parent=QWidget.find(rt.windows.getMAXHWND())):
+    def __init__(self, parent=None):
         # 继承父类的构造方法
         # 经典类的写法： 父类名称.__init__(self, 参数1，参数2，...)
         # 新式类的写法：super(子类，self).__init__(参数1，参数2，....)
         # 当testDialog被实例化之后，调用顺序  自己的__init__ ——> 父类__init__
-        QDialog.__init__(self, parent)#经典写法，继承父类的构造函数,也就是拥有父类的属性和方法
+        super(TestDialog, self).__init__(parent)#经典写法，继承父类的构造函数,也就是拥有父类的属性和方法
         loader = QUiLoader()#QuiLoder类 主要负责加载UI外部文件
         ui_file_path = os.path.join(  os.path.dirname(os.path.realpath(__file__)), 'ui/normal.ui')
-        ui_file = QFile(ui_file_path)
-        ui_file.open(QFile.ReadOnly)
-        self.ui = loader.load(ui_file, self)
+        ui_file = QFile(ui_file_path)#打开文件
+        ui_file.open(QFile.ReadOnly)#文件只读
+        self.ui = loader.load(ui_file, self)#导入ui内部的信息
         ui_file.close()
-
-        layout = QVBoxLayout()
-        layout.addWidget(self.ui)
-        self.setLayout(layout)
+        self.setWindowFlags(QtCore.Qt.Tool)#设置窗口属性，枚举
         self.setWindowTitle("技术中心_法线工具v1.0")
 
         # -------------------creat weight----------------
@@ -60,6 +58,7 @@ class TestDialog(QDialog):
         but_normal_select_3 = self.ui.findChild(QPushButton, 'but_normal_select_3')
 
         self.start_value()
+
         #---------------------------connect funcution------------------------
         but_display_normal.clicked.connect(self.display_normal)
         self.rad_one.toggled.connect(self.offset_value_rad)
@@ -98,8 +97,10 @@ class TestDialog(QDialog):
             self.offset_value_z_add = rt.quat(0, 0.0871557 / 10, 0, 0.999848)
             self.offset_value_z_sub = rt.quat(0, -0.0871557 / 10, 0, 0.999848)
 
+
     def normal_length(self):
         self.normal.displayLength = self.horizontalSlider.value()
+
 
     def display_normal(self):
         self.normal = rt.Edit_Normals()
@@ -107,6 +108,8 @@ class TestDialog(QDialog):
 
         for x in a :
             rt.addModifier(x,self.normal)
+
+        self.horizontalSlider.setValue(self.normal.displayLength)
         rt.redrawViews()
 
 
@@ -201,6 +204,8 @@ if __name__ == '__main__':
 
     except:
         pass
-    tool_window = TestDialog()
+    main_window2 = qtmax.GetQMaxMainWindow()
+    tool_window = TestDialog(parent=main_window2)
+    tool_window.setFloating(True)
     tool_window.show()
 
