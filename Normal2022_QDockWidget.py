8324de5dd750a1fd1b03ed42d2cfed1df112f2e1
fxcfxcfxc <49788446+fxcfxcfxc@users.noreply.github.com>
2022-03-02 19:33:10 +0800

    add

--- Normal2022_QDockWidget.py
+++ Normal2022_QDockWidget.py
@@ -16,6 +16,7 @@ from pymxs import runtime as rt
 
 '''
 
+
 #继承Qdialog类，代表对话框类
 class TestDialog(QDockWidget):
     #TestDialog类的构造函数，传入参数，参数为类类型
@@ -61,15 +62,17 @@ class TestDialog(QDockWidget):
 
         self.but_normal_strore_2 = self.ui.findChild(QPushButton, 'but_normal_strore_2')
         self.but_normal_select_2 = self.ui.findChild(QPushButton, 'but_normal_select_2')
+        self.btn_recover_normal2 = self.ui.findChild(QPushButton, 'btn_recover_normal2')
 
         self.but_normal_strore_3 = self.ui.findChild(QPushButton, 'but_normal_strore_3')
         self.but_normal_select_3 = self.ui.findChild(QPushButton, 'but_normal_select_3')
+        self.btn_recover_normal3 = self.ui.findChild(QPushButton, 'btn_recover_normal3')
 
         self.start_value()
 
     def creat_layout(self):
 
-        self.but_display_normal.clicked.connect(self.display_normal)
+        self.but_display_normal.clicked.connect(self.add_normal)
         self.rad_one.toggled.connect(self.offset_value_rad)
         self.rad_five.toggled.connect(self.offset_value_rad)
         self.rad_ten.toggled.connect(self.offset_value_rad)
@@ -88,15 +91,18 @@ class TestDialog(QDockWidget):
 
         self.but_normal_strore_2.clicked.connect(self.store_2)
         self.but_normal_select_2.clicked.connect(self.select_2)
+        self.btn_recover_normal2.clicked.connect(self.recover_2)
 
         self.but_normal_strore_3.clicked.connect(self.store_3)
         self.but_normal_select_3.clicked.connect(self.select_3)
+        self.btn_recover_normal3.clicked.connect(self.recover_3)
 
         self.horizontalSlider.valueChanged.connect(self.normal_length)
 
         self.resize(500, 550)
 
     def start_value(self):
+        #设置初始化偏移值，防止报错
         if (self.rad_one.isChecked()):
             # value  = 0.1
             self.offset_value_x_add = rt.Point3(0.05, 0, 0)
@@ -109,15 +115,17 @@ class TestDialog(QDockWidget):
             self.offset_value_z_sub = rt.Point3(0, 0, -0.05)
 
     def normal_length(self):
+        #设置修改器法线长度值等于滑块值
         self.normal.displayLength = self.horizontalSlider.value()
 
-    def display_normal(self):
+    def add_normal(self):
         self.normal = rt.Edit_Normals()
         a = rt.selection
 
         for x in a :
             rt.addModifier(x,self.normal)
 
+        #将滑块ui的值设置成法线修改器的默认值
         self.horizontalSlider.setValue(self.normal.displayLength)
         rt.redrawViews()
 
@@ -179,49 +187,89 @@ class TestDialog(QDockWidget):
             self.offset_value_z_add = rt.Point3(0,0,1)
             self.offset_value_z_sub = rt.Point3(0,0,-1)
 
+    def get_normal_array(self,bitArray):
+
+        # 暴露函数 bitarray to array
+        rt.execute("fn b2a b = (return b as Array)")
+        # 类型转换bitarray -》 array
+        array_select_normal = rt.b2a(bitArray)
+        return array_select_normal;
+
+    def store_normal(self,array):
+        # 法线向量数组存储
+        array_normal_dir = [];
+        for x in array:
+            normalTest = self.normal.GetNormal(x)  # 传入index，得到index法线对应的方向向量
+            array_normal_dir.append(normalTest)  #
+
+        return array_normal_dir;
+
     def store_1(self):
-        self.bitArray_sel_1 = self.normal.GetSelection()  # 得到选择的法线数组
+        self.bitArray_sel_1 = self.normal.GetSelection()#当前选择的法线索引存入 bitarray
+        self.normal_array_1 = self.get_normal_array(self.bitArray_sel_1)
+        self.normal_dir_1 = self.store_normal(self.normal_array_1)
 
-        # 数据类型转换
-        bitArray_sel_1 = self.normal.GetSelection()
-        rt.execute("fn b2a b = (return b as Array)")  # 强制使用maxscript  创建函数 bitarray to array
-        self.array_select_normal = rt.b2a(bitArray_sel_1)  # bitarray to array
+        '''
+        
+       
+        #暴露函数 bitarray to array
+        rt.execute("fn b2a b = (return b as Array)")
+        #类型转换bitarray -》 array
+        self.array_select_normal = rt.b2a(self.bitArray_sel_1)
 
-        # 迭代器
+        #法线向量数组存储
         self.array_normal_dir = []
         for x in self.array_select_normal:
             self.normalTest = self.normal.GetNormal(x)  # 传入index，得到index法线对应的方向向量
-            self.array_normal_dir.append(self.normalTest)  #
+            self.array_normal_dir.append(self.normalTest) #
+        '''
 
     def select_1(self):
 
-        self.normal_selection_1 = self.normal.SetSelection(self.bitArray_sel)
+        self.normal_selection_1 = self.normal.SetSelection(self.bitArray_sel_1)
 
     def recover_1(self):
         x = 0
-        for a in self.array_select_normal:
-            self.normal.SetNormal(a, self.array_normal_dir[x])
-            if (x <= len(self.array_select_normal)):
+        for a in self.normal_array_1:
+            self.normal.SetNormal(a, self.normal_dir_1[x])
+            if (x <= len(self.normal_array_1)):
                 x = x + 1
             rt.redrawViews()
 
     def store_2(self):
 
-        self.bitArray_sel_2 = self.normal.GetSelection()
+        self.bitArray_sel_2 = self.normal.GetSelection()  # 当前选择的法线索引存入 bitarray
+        self.normal_array_2 = self.get_normal_array(self.bitArray_sel_2)
+        self.normal_dir_2 = self.store_normal(self.normal_array_2)
 
-    def  select_2(self):
+    def select_2(self):
 
         self.normal_selection_2 = self.normal.SetSelection(self.bitArray_sel_2)
 
-    def store_3(self):
-        self.bitArray_sel_3 = self.normal.GetSelection()
+    def recover_2(self):
+        x = 0
+        for a in self.normal_array_2:
+            self.normal.SetNormal(a, self.normal_dir_2[x])
+            if (x <= len(self.normal_array_2)):
+                x = x + 1
+            rt.redrawViews()
 
-    def  select_3(self):
+    def store_3(self):
+        self.bitArray_sel_3 = self.normal.GetSelection()  # 当前选择的法线索引存入 bitarray
+        self.normal_array_3 = self.get_normal_array(self.bitArray_sel_3)
+        self.normal_dir_3 = self.store_normal(self.normal_array_3)
+
+    def select_3(self):
 
         self.normal_selection_3 = self.normal.SetSelection(self.bitArray_sel_3)
 
-
-
+    def recover_3(self):
+        x = 0
+        for a in self.normal_array_3:
+            self.normal.SetNormal(a, self.normal_dir_3[x])
+            if (x <= len(self.normal_array_3)):
+                x = x + 1
+            rt.redrawViews()
 
 
 
